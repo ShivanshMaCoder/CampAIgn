237354356fd64afb9da27db8deb5df308b57050a
ShivanshMathur007 <shivansh.mathur@incedoinc.com>
2024-07-01 14:27:49 +0530

    changes made

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ import firebase_admin
 from firebase_admin import credentials
 
 
-
-
-
 if not firebase_admin._apps:
     cred = credentials.Certificate({
         "type": st.secrets["firebase"]["type"],
